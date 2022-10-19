9e5cf2816c195d8e835b526eb72fa141ea2257de
Dmitriy Krasnov <d.krasnov2@g.nsu.ru>
2022-10-19 13:47:53 +0700

    fix style

--- Task_1_2_2/src/test/java/ru/nsu/peyuaa/TreeTest.java
+++ Task_1_2_2/src/test/java/ru/nsu/peyuaa/TreeTest.java
@@ -4,7 +4,11 @@
 
 package ru.nsu.peyuaa;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
