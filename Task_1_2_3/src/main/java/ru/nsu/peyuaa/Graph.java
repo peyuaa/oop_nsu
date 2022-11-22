802f29bb6167dd244eaa5890d931006ebfae45c7
Dmitriy Krasnov <itsnot@myemail.com>
2022-11-22 22:38:22 +0700

    added adjacency matrix support

--- Task_1_2_3/src/main/java/ru/nsu/peyuaa/Graph.java
+++ Task_1_2_3/src/main/java/ru/nsu/peyuaa/Graph.java
@@ -4,9 +4,8 @@
 
 package ru.nsu.peyuaa;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Graph<T> {
     private static class Vertex<T> {
@@ -30,19 +29,58 @@ public class Graph<T> {
         }
     }
 
+    private Map<Vertex<T>, Map<Vertex<T>, Integer>> adjacencyMatrix;
+
     private List<Vertex<T>> vertices;
     private List<Edge<T>> edges;
 
     public Graph() {
         vertices = new LinkedList<>();
         edges = new LinkedList<>();
+        adjacencyMatrix = new HashMap<>();
+    }
+
+    private void addVertexToVertices(Vertex<T> vertex) {
+        vertices.add(vertex);
+    }
+
+    private void addVertexToAdjacencyMatrix(Vertex<T> vertex) {
+        adjacencyMatrix.put(vertex, new HashMap<Vertex<T>, Integer>());
     }
 
     public void addVertex(T value) {
-        vertices.add(new Vertex<>(value));
+        Vertex<T> vertex = new Vertex<>(value);
+        addVertexToVertices(vertex);
+        addVertexToAdjacencyMatrix(vertex);
     }
 
-    public void deleteVertex(T value) {
+    public void addVertices(T[] values) {
+        for (T value : values) {
+            vertices.add(new Vertex<>(value));
+        }
+    }
+
+    public Vertex<T> getVertex(T value) {
+        for (Vertex<T> vertex : vertices) {
+            if (vertex.value.equals(value)) {
+                return vertex;
+            }
+        }
+        return null;
+    }
+
+    private void deleteVertexFromAdjacencyMatrix(T value) {
+        Vertex<T> vertex = getVertex(value);
+        adjacencyMatrix.remove(vertex);
+
+        for (Vertex<T> currentVertex : vertices) {
+            if (!currentVertex.equals(vertex)) {
+                adjacencyMatrix.get(currentVertex).remove(vertex);
+            }
+        }
+    }
+
+    private void deleteVertexFromVerticesList(T value) {
         for (Iterator<Vertex<T>> iterator = vertices.listIterator(); iterator.hasNext();) {
             Vertex<T> vertex = iterator.next();
             if (vertex.value.equals(value)) {
@@ -54,17 +92,39 @@ public class Graph<T> {
         }
     }
 
-    public void addEdge(int weight, Vertex<T> from, Vertex<T> to) {
+    public void deleteVertex(T value) {
+        deleteVertexFromAdjacencyMatrix(value);
+        deleteVertexFromVerticesList(value);
+    }
+
+    private void addEdgeToEdges(int weight, Vertex<T> from, Vertex<T> to) {
         Edge<T> edge = new Edge<>(weight, from, to);
         edges.add(edge);
         from.edges.add(edge);
         to.edges.add(edge);
     }
 
-    public void deleteEdge(Edge<T> edge) {
+    private void addEdgeInAdjacencyMatrix(int weight, Vertex<T> from, Vertex<T> to) {
+        adjacencyMatrix.get(from).put(to, weight);
+    }
+
+    public void addEdge(int weight, Vertex<T> from, Vertex<T> to) {
+        addEdgeToEdges(weight, from, to);
+        addEdgeInAdjacencyMatrix(weight, from, to);
+    }
+
+    private void deleteEdgeFromEdges(Edge<T> edge) {
         edge.from.edges.remove(edge);
         edge.to.edges.remove(edge);
         edges.remove(edge);
+    }
+
+    private void deleteEdgeFromAdjacencyMatrix(Edge<T> edge) {
+        adjacencyMatrix.get(edge.from).put(edge.to, 0);
+    }
+    public void deleteEdge(Edge<T> edge) {
+        deleteEdgeFromEdges(edge);
+        deleteEdgeFromAdjacencyMatrix(edge);
     }
 
     public T getValue(Vertex<T> vertex) {
@@ -75,11 +135,41 @@ public class Graph<T> {
         vertex.value = value;
     }
 
-    public void changeWeight(Edge<T> edge, int weight) {
+
+    private void changeWeightInEdges(Edge<T> edge, int weight) {
         edge.weight = weight;
+    }
+    private void changeWeightInAdjacencyMatrix(Edge<T> edge, int weight) {
+        adjacencyMatrix.get(edge.from).put(edge.to, weight);
+    }
+
+    public void changeWeight(Edge<T> edge, int weight) {
+        changeWeightInEdges(edge, weight);
+        changeWeightInAdjacencyMatrix(edge, weight);
     }
 
     public int getWeight(Edge<T> edge) {
         return edge.weight;
+    }
+
+    public void loadAdjacencyMatrix(String file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String currentLine = reader.readLine();
+
+        String[] verticesValues = currentLine.split(" ");
+        addVertices((T[]) verticesValues);
+
+        for (int i = 0 ; i < verticesValues.length; i++) {
+            currentLine = reader.readLine();
+            int[] weights = Arrays.stream(currentLine.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            Vertex<T> firstVertex = getVertex((T) verticesValues[i]);
+
+            for (int j = 0; j < weights.length; j++) {
+                if (weights[j] != 0) {
+                    addEdge(weights[j], firstVertex, getVertex((T) verticesValues[j]));
+                }
+            }
+        }
     }
 }
