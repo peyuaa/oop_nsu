74d4ccffbd35a4ec3f864e871ea4015dad849c01
peyuaa <peyuaa@users.noreply.github.com>
2022-12-05 05:16:03 +0000

    Deploying to gh-pages from @ peyuaa/oop_nsu@98ee932ad83eadfa696629c0cf7ac29ebe3d2d50 🚀

--- Task_1_5_1/member-search-index.js
+++ Task_1_5_1/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"ru.nsu.peyuaa","c":"Calculator","l":"Calculator()","url":"%3Cinit%3E()"},{"p":"ru.nsu.peyuaa","c":"Calculator","l":"getGreeting()"},{"p":"ru.nsu.peyuaa","c":"Calculator","l":"main(String[])","url":"main(java.lang.String[])"}]
+memberSearchIndex = [{"p":"ru.nsu.peyuaa","c":"Calculator","l":"Calculator()","url":"%3Cinit%3E()"}]
