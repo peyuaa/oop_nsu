ab057514939d0855aae189db6715d081b63962fc
Dmitriy Krasnov <itsnot@myemail.com>
2022-12-12 17:08:34 +0700

    fix style

--- Task_1_2_3/src/main/java/ru/nsu/peyuaa/Graph.java
+++ Task_1_2_3/src/main/java/ru/nsu/peyuaa/Graph.java
@@ -4,7 +4,11 @@
 
 package ru.nsu.peyuaa;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
