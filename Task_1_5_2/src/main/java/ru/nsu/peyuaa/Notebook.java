15aae868de4f52362d2f045890322873727e2bea
Dmitriy Krasnov <d.krasnov@protonmail.com>
2022-12-25 16:49:11 +0700

    mvp

--- Task_1_5_2/src/main/java/ru/nsu/peyuaa/Notebook.java
+++ Task_1_5_2/src/main/java/ru/nsu/peyuaa/Notebook.java
@@ -10,6 +10,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -47,8 +49,14 @@ public class Notebook {
     private final static String add = "-add";
     private final static String remove = "-rm";
     private final static String show = "-show";
-    private final static String whitespace = " ";
     private final static String fileName = "notebook.json";
+    private final static String inputDateFormat = "dd.MM.yyyy HH:mm";
+    private final static int keyWordsStartIndex = 3;
+    private final static int addNumberOfArguments = 3;
+    private final static int removeNumberOfArguments = 2;
+    private final static int showAllNumberOfArguments = 1;
+    private final static int showMinimalNumberOfArguments = 4;
+    private final static int minimalNumberOfArguments = 1;
     private final static ObjectMapper objectMapper = new ObjectMapper();
 
 
@@ -133,6 +141,17 @@ public class Notebook {
         }
     }
 
+    private void showNotes(String[] args) throws ParseException {
+        if (args.length == 1) {
+            printNotes();
+        } else {
+            SimpleDateFormat format = new SimpleDateFormat(inputDateFormat);
+            Date from = format.parse(args[1]);
+            Date to = format.parse(args[2]);
+            printNotes(from, to, Arrays.copyOfRange(args, keyWordsStartIndex, args.length));
+        }
+    }
+
     private void serialize() throws IOException {
         objectMapper.writeValue(new File(fileName), this);
     }
@@ -147,7 +166,63 @@ public class Notebook {
         return notebook;
     }
 
-    public static void main(String[] args) throws IOException {
+    private static boolean isAddValid(String[] args) {
+        return args.length == addNumberOfArguments;
+    }
+
+    private static boolean isRemoveValid(String[] args) {
+        return args.length == removeNumberOfArguments;
+    }
+
+    private static boolean isDateValid(String date) {
+        SimpleDateFormat format = new SimpleDateFormat(inputDateFormat);
+        Date parsedDate;
+        try {
+            parsedDate = format.parse(date);
+        } catch (ParseException e) {
+            return false;
+        }
+        return date.equals(format.format(parsedDate));
+    }
+
+    private static boolean isShowValid(String[] args) {
+        if (args.length == showAllNumberOfArguments) {
+            return true;
+        }
+
+        if (args.length < showMinimalNumberOfArguments) {
+            return false;
+        }
+        return isDateValid(args[1]) && isDateValid(args[2]);
+    }
+
+    private static boolean isInputValid(String[] args) {
+        if (args.length < minimalNumberOfArguments) {
+            return false;
+        }
+
+        return switch (args[0]) {
+            case add -> isAddValid(args);
+            case remove -> isRemoveValid(args);
+            case show -> isShowValid(args);
+            default -> false;
+        };
+    }
+
+    private void processInput(String[] args) throws IOException, ParseException {
+        switch (args[0]) {
+            case add -> addNote(args[1], args[2]);
+            case remove -> deleteNote(args[1]);
+            case show -> showNotes(args);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
         Notebook notebook = getNotebook();
+        if (isInputValid(args)) {
+            notebook.processInput(args);
+        } else {
+            System.err.println("Incorrect input");
+        }
     }
 }
