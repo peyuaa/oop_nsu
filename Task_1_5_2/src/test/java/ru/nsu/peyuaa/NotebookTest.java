3cc23045576cc419e15acc76eaeb32e6804f551a
Dmitriy Krasnov <d.krasnov@protonmail.com>
2022-12-26 13:36:16 +0700

    added test for showing all notes

--- Task_1_5_2/src/test/java/ru/nsu/peyuaa/NotebookTest.java
+++ Task_1_5_2/src/test/java/ru/nsu/peyuaa/NotebookTest.java
@@ -82,4 +82,48 @@ class NotebookTest {
             file.delete();
         }
     }
+
+    @Test
+    void showAll() throws IOException, ParseException {
+        try (
+                ByteArrayOutputStream outByte = new ByteArrayOutputStream();
+                PrintStream out = new PrintStream(outByte);
+                PrintStream err = new PrintStream(new ByteArrayOutputStream())
+        ) {
+            String fileName = "test.json";
+            File file = new File(fileName);
+            file.delete();
+
+            GregorianCalendar firstCalendar = (GregorianCalendar) GregorianCalendar.getInstance();
+            firstCalendar.clear();
+            firstCalendar.setTimeZone(TimeZone.getTimeZone("Asia/Novosibirsk"));
+            firstCalendar.set(2005, Calendar.APRIL, 13, 0, 0, 0);
+
+            GregorianCalendar secondCalendar = (GregorianCalendar) GregorianCalendar.getInstance();
+            secondCalendar.clear();
+            secondCalendar.setTimeZone(TimeZone.getTimeZone("Asia/Novosibirsk"));
+            secondCalendar.set(2009, Calendar.FEBRUARY, 13, 3, 7, 3);
+
+            Notebook.DateTime mockedDateImpl = mock();
+            when(mockedDateImpl.getDate()).thenReturn(firstCalendar.getTime(), secondCalendar.getTime());
+
+            String[] addFirstNote = new String[]{"-add", "my note", "to be honest I hate notes"};
+            Notebook.run(out, err, fileName, mockedDateImpl, addFirstNote);
+
+            String[] addSecondNote = new String[]{"-add", "my second note", "nvmd I love them"};
+            Notebook.run(out, err, fileName, mockedDateImpl, addSecondNote);
+
+            String[] showAll = new String[]{"-show"};
+            Notebook.run(out, err, fileName, mockedDateImpl, showAll);
+
+            String expected = "Note{title='my note', content='to be honest I hate notes',"
+                    + " created=Wed Apr 13 00:00:00 NOVST 2005}\n" +
+                    "Note{title='my second note', content='nvmd I love them', "
+                    + "created=Fri Feb 13 03:07:03 NOVT 2009}\n";
+
+            Assertions.assertEquals(expected, outByte.toString());
+
+            file.delete();
+        }
+    }
 }
