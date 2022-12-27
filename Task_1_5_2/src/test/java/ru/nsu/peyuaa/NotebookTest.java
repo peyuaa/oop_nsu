64264628bb9238476eef0c564cffe5e2ce55353d
Dmitriy Krasnov <d.krasnov@protonmail.com>
2022-12-27 22:03:38 +0700

    added test for showing specific notes

--- Task_1_5_2/src/test/java/ru/nsu/peyuaa/NotebookTest.java
+++ Task_1_5_2/src/test/java/ru/nsu/peyuaa/NotebookTest.java
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.Test;
 
 class NotebookTest {
     @Test
-    void addNote() throws IOException {
+    void addNote() throws IOException, ParseException {
         try (
                 PrintStream out = new PrintStream(new ByteArrayOutputStream());
                 PrintStream err = new PrintStream(new ByteArrayOutputStream())
@@ -52,7 +52,7 @@ class NotebookTest {
     }
 
     @Test
-    void removeNote() throws IOException {
+    void removeNote() throws IOException, ParseException {
         try (
                 PrintStream out = new PrintStream(new ByteArrayOutputStream());
                 PrintStream err = new PrintStream(new ByteArrayOutputStream())
@@ -84,7 +84,7 @@ class NotebookTest {
     }
 
     @Test
-    void showAll() throws IOException {
+    void showAll() throws IOException, ParseException {
         try (
                 ByteArrayOutputStream outByte = new ByteArrayOutputStream();
                 PrintStream out = new PrintStream(outByte);
@@ -121,6 +121,59 @@ class NotebookTest {
                     + " created=13.04.2005 00:00:00 +0700}\n"
                     + "Note{title='my second note', content='nvmd I love them', "
                     + "created=13.02.2009 03:07:03 +0600}\n";
+
+            Assertions.assertEquals(expected, outByte.toString());
+
+            file.delete();
+        }
+    }
+
+    @Test
+    void showSpecified() throws IOException, ParseException {
+        try (
+                ByteArrayOutputStream outByte = new ByteArrayOutputStream();
+                PrintStream out = new PrintStream(outByte);
+                PrintStream err = new PrintStream(new ByteArrayOutputStream())
+        ) {
+            String fileName = "test.json";
+            File file = new File(fileName);
+            file.delete();
+
+            GregorianCalendar firstCalendar = (GregorianCalendar) GregorianCalendar.getInstance();
+            firstCalendar.clear();
+            firstCalendar.setTimeZone(TimeZone.getTimeZone("Asia/Novosibirsk"));
+            firstCalendar.set(2005, Calendar.APRIL, 13, 0, 0, 0);
+
+            GregorianCalendar secondCalendar = (GregorianCalendar) GregorianCalendar.getInstance();
+            secondCalendar.clear();
+            secondCalendar.setTimeZone(TimeZone.getTimeZone("Asia/Novosibirsk"));
+            secondCalendar.set(2009, Calendar.FEBRUARY, 13, 3, 7, 3);
+
+            GregorianCalendar thirdCalendar = (GregorianCalendar) GregorianCalendar.getInstance();
+            thirdCalendar.clear();
+            thirdCalendar.setTimeZone(TimeZone.getTimeZone("Asia/Novosibirsk"));
+            thirdCalendar.set(2009, Calendar.FEBRUARY, 13, 3, 8, 0);
+
+            Notebook.DateTime mockedDateImpl = mock();
+            when(mockedDateImpl.getDate()).thenReturn(firstCalendar.getTime(),
+                    secondCalendar.getTime(), thirdCalendar.getTime());
+
+            String[] addFirstNote = new String[]{"-add", "my note", "to be honest I hate notes"};
+            Notebook.run(out, err, fileName, mockedDateImpl, addFirstNote);
+
+            String[] addSecondNote = new String[]{"-add", "my second note", "nvmd I love them"};
+            Notebook.run(out, err, fileName, mockedDateImpl, addSecondNote);
+
+            String[] addThirdNote = new String[]{"-add", "big secret", "I'm insane"};
+            Notebook.run(out, err, fileName, mockedDateImpl, addThirdNote);
+
+            String[] show = new String[]{"-show", "13.02.2009 03:07", "13.02.2009 03:09", "secret", "note"};
+            Notebook.run(out, err, fileName, mockedDateImpl, show);
+
+            String expected = "Note{title='my second note', content='nvmd I love them',"
+                    + " created=13.02.2009 03:07:03 +0600}\n" +
+                    "Note{title='big secret', content='I'm insane', "
+                    + "created=13.02.2009 03:08:00 +0600}\n";
 
             Assertions.assertEquals(expected, outByte.toString());
 
