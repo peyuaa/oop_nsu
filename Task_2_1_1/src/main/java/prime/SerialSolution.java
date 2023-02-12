859ddd2dad21572abca1e8d850b73b679f7f7b73
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-02-13 03:03:22 +0700

    linter warnings

--- Task_2_1_1/src/main/java/prime/SerialSolution.java
+++ Task_2_1_1/src/main/java/prime/SerialSolution.java
@@ -4,18 +4,23 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 /**
- * The class {@code SerialSolution} contains a single public method {@link #containsNonPrime(int[])}
- * which takes in an integer array and returns a boolean indicating if the array contains a non-prime number.
+ * The class {@code SerialSolution} has a single public method,
+ * {@link #containsNonPrime(int[])}, which takes an integer array and
+ * returns a boolean indicating if the array contains a non-prime number.
  * <p>
- * The method first finds the maximum element in the array and creates a boolean array to store the prime numbers
- * up to the maximum element. The boolean array is initialized with all elements set to true and the first two
- * elements, 0 and 1, set to false since they are not primes.
+ * The method first finds the max element in the array and creates a boolean
+ * array to store the prime numbers up to the max. The boolean array is
+ * initialized with all elements set to true and the first two elements, 0 and
+ * 1, set to false (since they are not primes).
  * <p>
- * The method then uses the Sieve of Eratosthenes algorithm to fill the boolean array with the prime numbers.
- * It iterates through the numbers from 2 to the square root of the maximum element, filtering out the numbers
- * that are not prime. For each prime number, the method sets all its multiples as non-prime in the boolean array.
+ * The method then uses the Sieve of Eratosthenes algorithm to fill the
+ * boolean array with the prime numbers. It iterates through the numbers
+ * from 2 to the sqrt of the max element, filtering out the non-prime numbers.
+ * For each prime number, the method sets all its multiples as non-prime in
+ * the boolean array.
  * <p>
- * Finally, the method returns true if the array contains any number that is not marked as prime in the boolean array.
+ * Finally, the method returns true if the array contains any number that is
+ * not marked as prime in the boolean array.
  */
 public class SerialSolution {
 
