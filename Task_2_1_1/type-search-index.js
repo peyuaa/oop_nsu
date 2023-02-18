1f3f39876aa65a293e8134791959a528fd950e4b
peyuaa <peyuaa@users.noreply.github.com>
2023-02-18 06:17:45 +0000

    Deploying to gh-pages from @ peyuaa/oop_nsu@411ece82a52625c3e7e5bfc0e0333c7e2052f4dc 🚀

--- Task_2_1_1/type-search-index.js
+++ Task_2_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"prime","l":"ParallelSolution"},{"p":"prime","l":"SerialSolution"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"prime","l":"ParallelSolution"},{"p":"prime","l":"ParallelStreamSolution"},{"p":"prime","l":"SerialSolution"}];updateSearchResults();
