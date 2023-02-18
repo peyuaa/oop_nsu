dd9c25e1e384f3c0435650ec71cd67cc673db55e
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-02-18 13:16:46 +0700

    solution using parallel stream

---
+++ Task_2_1_1/src/main/java/prime/ParallelStreamSolution.java
@@ -0,0 +1,25 @@
+package prime;
+
+import java.util.Arrays;
+
+public class ParallelStreamSolution {
+
+    public boolean isNonPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsNonPrime(int[] arr) {
+        return Arrays.stream(arr)
+                .parallel()
+                .anyMatch(this::isNonPrime);
+    }
+}
+
