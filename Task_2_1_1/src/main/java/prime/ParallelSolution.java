8b6b7033a5e5e7e7bb97be666b6276c96fe08fde
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-02-18 13:06:28 +0700

    updated algorithm for parallel solution using Thread

---
+++ Task_2_1_1/src/main/java/prime/ParallelSolution.java
@@ -0,0 +1,77 @@
+package prime;
+
+import java.lang.Math;
+
+public class ParallelSolution {
+    private int numThreads;
+
+    public ParallelSolution(int numThreads) {
+        this.numThreads = numThreads;
+    }
+
+    public boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean containsNonPrime(int[] arr) throws InterruptedException {
+        int chunkSize = (int) Math.ceil((double) arr.length / numThreads);
+        CheckPrimeThread[] threads = new CheckPrimeThread[numThreads];
+
+        // Create and start the threads
+        for (int i = 0; i < numThreads; i++) {
+            int startIndex = i * chunkSize;
+            int endIndex = Math.min((i + 1) * chunkSize, arr.length);
+            threads[i] = new CheckPrimeThread(arr, startIndex, endIndex);
+            threads[i].start();
+        }
+
+        // Wait for all threads to finish and collect their results
+        boolean hasNonPrime = false;
+        for (int i = 0; i < numThreads; i++) {
+            threads[i].join();
+            if (threads[i].hasNonPrime()) {
+                hasNonPrime = true;
+            }
+        }
+        return hasNonPrime;
+    }
+
+    private class CheckPrimeThread extends Thread {
+        private int[] arr;
+        private int startIndex;
+        private int endIndex;
+        private boolean nonPrimeFound;
+
+        public CheckPrimeThread(int[] arr, int startIndex, int endIndex) {
+            this.arr = arr;
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
+        }
+
+        public boolean hasNonPrime() {
+            return nonPrimeFound;
+        }
+
+        public void run() {
+            for (int i = startIndex; i < endIndex; i++) {
+                int num = arr[i];
+                if (num == 2) {
+                    continue;
+                }
+                if (num % 2 == 0 || !isPrime(num)) {
+                    nonPrimeFound = true;
+                    break;
+                }
+            }
+        }
+    }
+}
+
---
+++ Task_2_1_1/src/test/java/prime/ParallelSolutionTest.java
@@ -0,0 +1,66 @@
+package prime;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParallelSolutionTest {
+    private ParallelSolution parallelSolution = new ParallelSolution(2);
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, true",
+            "3, true",
+            "4, false",
+            "5, true",
+            "6, false",
+            "7, true",
+            "8, false",
+            "9, false",
+            "10, false",
+            "11, true",
+            "12, false",
+            "13, true",
+            "14, false",
+            "15, false",
+            "16, false",
+            "17, true",
+            "18, false",
+            "19, true",
+            "20, false",
+            "21, false",
+            "22, false",
+            "23, true",
+            "24, false",
+            "25, false",
+            "26, false",
+            "27, false",
+            "28, false",
+            "29, true",
+            "30, false",
+            "31, true"
+    })
+    public void testIsNonPrime(int num, boolean expected) {
+        assertEquals(expected, parallelSolution.isPrime(num));
+    }
+
+    @Test
+    public void testHasNonPrime() throws InterruptedException {
+        int[] arr1 = {2, 3, 5, 7, 11, 13};
+        assertFalse(parallelSolution.containsNonPrime(arr1));
+
+        int[] arr2 = {2, 3, 4, 5, 7, 11, 13};
+        assertTrue(parallelSolution.containsNonPrime(arr2));
+
+        int[] arr3 = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+        assertFalse(parallelSolution.containsNonPrime(arr3));
+
+        int[] arr4 = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 30};
+        assertTrue(parallelSolution.containsNonPrime(arr4));
+
+        int[] arr5 = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+        assertFalse(parallelSolution.containsNonPrime(arr5));
+    }
+}
+
