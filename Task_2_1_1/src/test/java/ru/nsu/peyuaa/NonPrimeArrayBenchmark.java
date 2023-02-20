8f3bc6fc518dabe8a4609f2657743e32972d20fa
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-02-20 20:10:37 +0700

    javadoc

--- Task_2_1_1/src/test/java/ru/nsu/peyuaa/NonPrimeArrayBenchmark.java
+++ Task_2_1_1/src/test/java/ru/nsu/peyuaa/NonPrimeArrayBenchmark.java
@@ -31,6 +31,13 @@ public class NonPrimeArrayBenchmark {
     private static final int THREAD_COUNT = 4;
     private static int[] array;
 
+    /**
+     * Initializes the static integer array with random integer values between 1 and ARRAY_SIZE.
+     * The method is annotated with @BeforeAll to run before all benchmark tests in the class.
+     * It creates a new integer array of size ARRAY_SIZE, initializes a new Random object,
+     * and uses a for loop to fill the array with random integer values between 1 and ARRAY_SIZE
+     * using the Random object's nextInt() method.
+     */
     @BeforeAll
     public static void init() {
         array = new int[ARRAY_SIZE];
@@ -61,6 +68,12 @@ public class NonPrimeArrayBenchmark {
         parallelStreamSolution.containsNonPrime(array);
     }
 
+    /**
+     * Runs the benchmark tests for the NonPrimeArrayBenchmark class using the JMH Runner class.
+     * The method creates a new Options object with the class name as the include pattern and uses it to build a
+     * new OptionsBuilder object. It then creates a new Runner object with the OptionsBuilder object
+     * and runs the benchmark tests. The method throws a RunnerException if the benchmark tests fail to run.
+     */
     public void runBenchmark() throws RunnerException {
         Options opt = new OptionsBuilder()
                 .include(NonPrimeArrayBenchmark.class.getSimpleName())
