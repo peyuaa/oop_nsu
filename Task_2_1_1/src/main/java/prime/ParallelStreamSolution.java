73a4bcb3babd67451f9a7efbadad41660c611a97
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-02-20 19:30:12 +0700

    javadoc for parallel stream solution

--- Task_2_1_1/src/main/java/prime/ParallelStreamSolution.java
+++ Task_2_1_1/src/main/java/prime/ParallelStreamSolution.java
@@ -2,8 +2,17 @@ package prime;
 
 import java.util.Arrays;
 
+/**
+ * The ParallelStreamSolution class provides a parallelized solution for determining whether an integer array
+ * contains any non-prime numbers. It uses Java 8 Streams to parallelize the checking process.
+ */
 public class ParallelStreamSolution {
 
+    /**
+     * Determines whether a given integer is a non-prime number.
+     * @param num the integer to check
+     * @return true if num is a non-prime number, false otherwise
+     */
     public boolean isNonPrime(int num) {
         if (num <= 1) {
             return false;
@@ -16,6 +25,11 @@ public class ParallelStreamSolution {
         return false;
     }
 
+    /**
+     * Determines whether an integer array contains any non-prime numbers, using Java 8 Streams.
+     * @param arr the integer array to check
+     * @return true if the array contains at least one non-prime number, false otherwise
+     */
     public boolean containsNonPrime(int[] arr) {
         if (arr == null) {
             return false;
