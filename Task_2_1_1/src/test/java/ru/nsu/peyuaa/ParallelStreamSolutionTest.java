8311e4f3e9e48ecec3e38cea77251c5563558e8a
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-02-27 20:02:02 +0700

    deleted unused imports

--- Task_2_1_1/src/test/java/ru/nsu/peyuaa/ParallelStreamSolutionTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/peyuaa/ParallelStreamSolutionTest.java
@@ -1,12 +1,8 @@
 package ru.nsu.peyuaa;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class ParallelStreamSolutionTest {
     private ParallelStreamSolution parallelStreamSolution = new ParallelStreamSolution();
