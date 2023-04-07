6b888bcedf0d7dfcc8ea9eb002d5e3deaa013880
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-07 16:51:26 +0700

    added javadoc for Customer

--- Task_2_2_1/src/main/java/ru/nsu/peyuaa/Customer.java
+++ Task_2_2_1/src/main/java/ru/nsu/peyuaa/Customer.java
@@ -1,17 +1,35 @@
 package ru.nsu.peyuaa;
 
+/**
+ * A `Customer` represents a customer who wants to order a pizza from a `Pizzeria`.
+ * Each `Customer` is run as a separate thread.
+ */
 public class Customer extends Thread {
 
+    /**
+     * The `Pizzeria` from which the customer wants to order a pizza.
+     */
     private final Pizzeria pizzeria;
 
+    /**
+     * Constructs a new `Customer` object with the specified `Pizzeria`.
+     *
+     * @param pizzeria the `Pizzeria` from which the customer wants to order a pizza
+     */
     public Customer(Pizzeria pizzeria) {
         this.pizzeria = pizzeria;
     }
 
+    /**
+     * Tells the `Pizzeria` to make an order for a new `Pizza`.
+     */
     public void makeOrder() {
         pizzeria.makeAnOrder(new Pizza());
     }
 
+    /**
+     * Starts the `Customer` thread and tells it to make an order.
+     */
     public void run() {
         System.out.println("Customer started");
         makeOrder();
