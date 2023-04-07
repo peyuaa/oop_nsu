81e9c74ac4b6f7f255fcb07c379c871c089882f6
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-07 17:28:25 +0700

    sort imports in lexicographical order

--- Task_2_2_1/src/main/java/ru/nsu/peyuaa/Main.java
+++ Task_2_2_1/src/main/java/ru/nsu/peyuaa/Main.java
@@ -5,12 +5,12 @@
 package ru.nsu.peyuaa;
 
 import com.google.gson.Gson;
-import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.io.FileUtils;
 
 /**
  * This class represents the entry point of the pizza delivery simulation program.
