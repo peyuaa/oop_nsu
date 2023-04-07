d005daf1b4d219f23db0dbd0df3832b0158ade0c
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-07 16:56:27 +0700

    added javadoc for Config

--- Task_2_2_1/src/main/java/ru/nsu/peyuaa/Config.java
+++ Task_2_2_1/src/main/java/ru/nsu/peyuaa/Config.java
@@ -1,12 +1,50 @@
 package ru.nsu.peyuaa;
+
+/**
+ * A `Config` represents the configuration settings for a pizzeria.
+ */
 public class Config {
+
+    /**
+     * The number of `Baker`s to employ.
+     */
     public int bakersNum;
+
+    /**
+     * The time it takes for a `Baker` to cook a pizza, in milliseconds.
+     */
     public int bakersTimeToCook;
+
+    /**
+     * The number of `Courier`s to employ.
+     */
     public int couriersNum;
+
+    /**
+     * The maximum number of pizzas that a `Courier` can deliver at once.
+     */
     public int couriersMaxVolume;
+
+    /**
+     * The number of `Customer`s to simulate.
+     */
     public int customersNum;
+
+    /**
+     * The maximum number of orders that the `Warehouse` can hold.
+     */
     public int warehouseSize;
 
+    /**
+     * Constructs a new `Config` object with the specified settings.
+     *
+     * @param bakersNum the number of `Baker`s to employ
+     * @param bakersTimeToCook the time it takes for a `Baker` to cook a pizza, in milliseconds
+     * @param couriersNum the number of `Courier`s to employ
+     * @param couriersMaxVolume the maximum number of pizzas that a `Courier` can deliver at once
+     * @param customersNum the number of `Customer`s to simulate
+     * @param warehouseSize the maximum number of orders that the `Warehouse` can hold
+     */
     public Config(int bakersNum, int bakersTimeToCook, int couriersNum,
                     int couriersMaxVolume, int customersNum, int warehouseSize) {
         this.bakersNum = bakersNum;
