f46e74fd26974a7b1ed3e9f14eced30a27992904
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-07 17:06:33 +0700

    added javadoc for OrderState

--- Task_2_2_1/src/main/java/ru/nsu/peyuaa/OrderState.java
+++ Task_2_2_1/src/main/java/ru/nsu/peyuaa/OrderState.java
@@ -1,10 +1,14 @@
 package ru.nsu.peyuaa;
 
+/**
+ * An enumeration representing the different states an order can be in:
+ * QUEUED, COOKING, COOKED, WAREHOUSE, DELIVERING, and DELIVERED.
+ */
 public enum OrderState {
-    QUEUED,
-    COOKING,
-    COOKED,
-    WAREHOUSE,
-    DELIVERING,
-    DELIVERED
+    QUEUED, // The order is in the queue
+    COOKING, // The order is being cooked
+    COOKED, // The order has been cooked
+    WAREHOUSE, // The order is in the warehouse
+    DELIVERING, // The order is being delivered
+    DELIVERED // The order has been delivered
 }
