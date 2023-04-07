79b100c42a2299306eecc158ae2714438993fecd
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-07 17:00:38 +0700

    added javadoc for Courier

--- Task_2_2_1/src/main/java/ru/nsu/peyuaa/Courier.java
+++ Task_2_2_1/src/main/java/ru/nsu/peyuaa/Courier.java
@@ -3,23 +3,60 @@ package ru.nsu.peyuaa;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A `Courier` represents an employee of a pizzeria who is responsible for delivering pizzas to customers.
+ */
 public class Courier extends Thread {
+
+    /**
+     * The number of `Courier`s created so far.
+     */
     private static int courierCount = 0;
+
+    /**
+     * The maximum number of pizzas that this `Courier` can deliver at once.
+     */
     private final int maxVolume;
+
+    /**
+     * The `Warehouse` from which this `Courier` picks up pizzas.
+     */
     private final Warehouse warehouse;
+
+    /**
+     * The list of orders that this `Courier` is currently delivering.
+     */
     private final List<Order> orders;
 
+    /**
+     * Constructs a new `Courier` object with the specified settings.
+     *
+     * @param maxVolume the maximum number of pizzas that this `Courier` can deliver at once
+     * @param warehouse the `Warehouse` from which this `Courier` picks up pizzas
+     */
     public Courier(int maxVolume, Warehouse warehouse) {
         this.maxVolume = maxVolume;
         this.warehouse = warehouse;
         this.orders = new ArrayList<>();
     }
 
+    /**
+     * Adds the specified `Order` to this `Courier`'s list of orders to deliver.
+     * Sets the state of the `Order` to `DELIVERING`.
+     *
+     * @param order the `Order` to add
+     */
     public void addOrder(Order order) {
         order.setState(OrderState.DELIVERING);
         orders.add(order);
     }
 
+    /**
+     * Runs this `Courier`. Loops indefinitely,
+     * picking up pizzas from the `Warehouse` and delivering them to customers.
+     * Once an order has been delivered, sets the state of the `Order` to `DELIVERED`.
+     * Sleeps for 5 seconds between deliveries.
+     */
     @Override
     public void run() {
         System.out.println("Courier " + courierCount++ + " is ready");
