e7aa281631c348ef2e7f66609df7c94ad0f2d494
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-07 17:11:51 +0700

    added javadoc for Pizza class

--- Task_2_2_1/src/main/java/ru/nsu/peyuaa/Pizza.java
+++ Task_2_2_1/src/main/java/ru/nsu/peyuaa/Pizza.java
@@ -1,6 +1,14 @@
 package ru.nsu.peyuaa;
 
+/**
+ * The Pizza class represents a pizza object.
+ * This class has a default constructor which creates a new Pizza object.
+ */
 public class Pizza {
+
+    /**
+     * Constructs a new Pizza object.
+     */
     public Pizza() {
     }
 }
