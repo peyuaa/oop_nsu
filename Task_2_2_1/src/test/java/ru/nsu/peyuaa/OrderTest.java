dc3a2b948c2c9db589867ffeacef381381862253
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-07 19:22:25 +0700

    added OrderTest

---
+++ Task_2_2_1/src/test/java/ru/nsu/peyuaa/OrderTest.java
@@ -0,0 +1,33 @@
+package ru.nsu.peyuaa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderTest {
+
+    @Test
+    public void testNewOrderCreatedWithQueuedState() {
+        Pizza pizza = new Pizza();
+        Order order = new Order(pizza);
+        Assertions.assertEquals(OrderState.QUEUED, order.getState());
+    }
+
+    @Test
+    public void testNewOrderCreatedWithUniqueID() {
+        Pizza pizza1 = new Pizza();
+        Order order1 = new Order(pizza1);
+
+        Pizza pizza2 = new Pizza();
+        Order order2 = new Order(pizza2);
+
+        Assertions.assertNotEquals(order1.getOrderId(), order2.getOrderId());
+    }
+
+    @Test
+    public void testGenerateOrderIdMethodGeneratesUniqueIDs() {
+        Order order1 = new Order(new Pizza());
+        Order order2 = new Order(new Pizza());
+        Assertions.assertEquals(order1.getOrderId() + 1, order2.getOrderId());
+    }
+}
+
