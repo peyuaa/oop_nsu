a5e65303274f8a3c671b1d54e54770e8f7ddf1fc
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-10 14:13:00 +0700

    fix test

--- Task_2_2_1/src/test/java/ru/nsu/peyuaa/BakerTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/peyuaa/BakerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BakerTest {
+class BakerTest {
     @Test
     public void testMakePizza() {
         Warehouse warehouse = new Warehouse(10);
