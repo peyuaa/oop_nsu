594f83808df9238f77a4b954666a6e9d5088df10
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-10 14:22:57 +0700

    linter

--- Task_2_2_1/src/test/java/ru/nsu/peyuaa/PizzeriaTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/peyuaa/PizzeriaTest.java
@@ -1,11 +1,13 @@
 package ru.nsu.peyuaa;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PizzeriaTest {
     @Test
