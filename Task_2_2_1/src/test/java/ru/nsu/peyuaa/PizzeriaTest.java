0210245919eb9321433142deb8e8016e77ecb6a8
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-10 14:20:13 +0700

    added pizzeria test

---
+++ Task_2_2_1/src/test/java/ru/nsu/peyuaa/PizzeriaTest.java
@@ -0,0 +1,59 @@
+package ru.nsu.peyuaa;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PizzeriaTest {
+    @Test
+    void testMakeAnOrder() {
+        Warehouse warehouse = new Warehouse(10);
+        Pizzeria pizzeria = new Pizzeria(warehouse);
+
+        // Make an order and check that it was added to the orders queue
+        pizzeria.makeAnOrder(new Pizza());
+        Order order = pizzeria.getOrder();
+        assertNotNull(order);
+        assertEquals(OrderState.QUEUED, order.getState());
+        assertNull(pizzeria.getOrder());
+    }
+
+    @Test
+    void testSetBakers() throws InterruptedException {
+        Warehouse warehouse = new Warehouse(10);
+        Pizzeria pizzeria = new Pizzeria(warehouse);
+
+        // Set the list of bakers and check that they are all started
+        List<Baker> bakers = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            bakers.add(new Baker(pizzeria, 100));
+        }
+        pizzeria.setBakers(bakers);
+        pizzeria.start();
+        Thread.sleep(2000);
+        for (Baker baker : bakers) {
+            assertTrue(baker.isAlive());
+        }
+    }
+
+    @Test
+    void testSetCouriers() throws InterruptedException {
+        Warehouse warehouse = new Warehouse(1);
+        Pizzeria pizzeria = new Pizzeria(warehouse);
+
+        // Set the list of couriers and check that they are all started
+        List<Courier> couriers = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            couriers.add(new Courier(1, 100, warehouse));
+        }
+        pizzeria.setCouriers(couriers);
+        pizzeria.start();
+        Thread.sleep(2000);
+        for (Courier courier : couriers) {
+            assertTrue(courier.isAlive());
+        }
+    }
+}
