2ec8c66d776d53be9db9b620af9cb2fbba3c0586
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-10 14:10:18 +0700

    BakerTest

---
+++ Task_2_2_1/src/test/java/ru/nsu/peyuaa/BakerTest.java
@@ -0,0 +1,18 @@
+package ru.nsu.peyuaa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BakerTest {
+    @Test
+    public void testMakePizza() {
+        Warehouse warehouse = new Warehouse(10);
+        Pizzeria pizzeria = new Pizzeria(warehouse);
+        Baker baker = new Baker(pizzeria, 500);
+        Order order = new Order(new Pizza());
+        baker.makePizza(order);
+        assertEquals(OrderState.COOKED, order.getState());
+    }
+
+}
