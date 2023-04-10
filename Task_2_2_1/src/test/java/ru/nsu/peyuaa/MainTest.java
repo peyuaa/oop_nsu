1e5cd3457f070d65b11f145eed0c321498f8619d
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-10 17:18:02 +0700

    MainTest

---
+++ Task_2_2_1/src/test/java/ru/nsu/peyuaa/MainTest.java
@@ -0,0 +1,12 @@
+package ru.nsu.peyuaa;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class MainTest {
+    @Test
+    void testMain() throws IOException {
+        Main.main(new String[] {});
+    }
+}
