015d5c2bb0227907dca859cb128120f801b69604
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-04-21 19:40:08 +0700

    delete unused import

--- Task_2_2_1/src/test/java/ru/nsu/peyuaa/PizzeriaTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/peyuaa/PizzeriaTest.java
@@ -2,7 +2,6 @@ package ru.nsu.peyuaa;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
